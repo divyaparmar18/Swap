467e4cacb199926d165638951c984be7b2ae9d44
divya <divya18@navgurukul.org>
2019-05-30 14:48:46 +0530

    add . swap in Integer

--- Swapper.java
+++ Swapper.java
@@ -1,18 +1,19 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Swapper {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(2,5,6,4,7,3,5);
-        System.out.println(getNumbersSwapped(numbers,0,5));
-
+        List<Integer> numbers = Arrays.asList(2,5,6,4,7,3,9);
+        System.out.println(getNumbersSwapped(numbers,0,6));
+        System.out.println(numbers);
     }
     public static List<Integer> getNumbersSwapped(List<Integer> numbers,int i,int j){
-        Integer index1 = numbers.get(i);
-        Integer index2 = numbers.get(j);
-        numbers.set(j,index1);
-        numbers.set(i,index2);
-        return numbers;
+        List<Integer> copyOfNumbers = new ArrayList<>(numbers);
+        int index2 = copyOfNumbers.get(j);
+        copyOfNumbers.set(j,copyOfNumbers.get(i));
+        copyOfNumbers.set(i,index2);
+        return copyOfNumbers;
     }
 
 }
